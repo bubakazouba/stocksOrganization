6b67ba07fb21f03cda0c2744a325e8f56ec38190
Abdulrahman Sahmoud <slashhashdash@gmail.com>
2014-05-28 06:06:23 +0000

    Stocks.h edited online with Bitbucket

--- Stocks.h
+++ Stocks.h
@@ -1,19 +1,22 @@
+//Name: Abdulrahman Sahmoud
+#include <string>
 #ifndef _STOCKS_H_
 #define _STOCKS_H_
 
 const MAX_DAYS=30;
 class Stocks{
 private:
-int Ndays;//number of days for that company(will calculate that from the 
-sring TickerSymbol;
-string Name;
-Double[MAX_DAYS] Prices;
-Double[MAX_DAYS] High;
-Double[MAX_DAYS] Low;
-unsigned long long[MAX_DAYS] MarketCap;//why is that a long long not a double?
+	//Ndays wont have a setter
+int Ndays;//number of days for that company(will calculate that from the line im going to get)
+string TickerSymbol;
+string name;
+Double[MAX_DAYS] prices;
+Double[MAX_DAYS] high;
+Double[MAX_DAYS] low;
+unsigned long long[MAX_DAYS] marketCap;//why is that a long long not a double?
 Doube[MAX_DAYS] open;
-unsigned long long Volume;
-Double[MAX_DAYS] Close;
+unsigned long long[MAX_DAYS]volume;
+Double[MAX_DAYS] close;
 
 
 public:
@@ -25,6 +28,18 @@ operator ==(const Stocks& rhs);
 operator > (const Stocks& rhs);
 operator < (const Stocks& rhs);
 operator = (const Stocks& rhs);
-
+operator != (const Stocks& rhs);
+///////////////////GETTERS////////////////////////////////
+string getTickerSymbol()const {return TickerSymbol;}
+string getName()const {return name;}
+//make sure in the function that the index is in bounds//
+bool getPrice(double & ret,int ind){}
+bool getHigh(double & ret,int ind){}
+bool getLow(double & ret,int ind){}
+bool getOpen(double & ret,int ind){}
+bool getClose(double & ret,int ind){}
+bool getmarketCap(long long & ret,int ind){}
+bool getVolume(long long & ret,int ind){}
+/////////////////////////////////////////////////////////
 };
 #endif
