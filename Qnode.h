b934aeff7063c9ba18d9186af1f811d83cb1c95e
Inaki Gonzalo <nacho63@users.noreply.github.com>
2014-05-30 11:15:21 -0700

    Qnode fixed

--- Qnode.h
+++ Qnode.h
@@ -9,21 +9,21 @@ Qnode
 template<class ItemType>
 class Qnode{
 private:
-	ItemType data;
+	ItemType* data;
 	Qnode* next;
 
 public:
-	Qnode(){ next = 0; prev = 0;}
-	Qnode(ItemType newdata){ data = newdata; prev = 0; next = 0;}
+	Qnode(){ next = 0; }
+	Qnode(ItemType* newdata){ data = newdata; next = 0;}
 	
 //		
 
-	getNext(){ return next; }
-	setNext(Qnode* newnext){ next = newnext; }
+	Qnode* getNext(){ return next; }
+	void setNext(Qnode* newnext){ next = newnext; }
 
-	getData(){ return data;}
-	setData(ItemType newdata){ data = newdata; }
+	ItemType* getData(){ return data;}
+	void setData(ItemType* newdata){ data = newdata; }
 
-}
+};
 
 #endif
