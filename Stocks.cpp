3dd03712e407e87a09194d13c154d94268ef718b
Abdulrahman Sahmoud <slashhashdash@gmail.com>
2014-06-02 23:26:41 +0000

    Stocks.cpp edited online with Bitbucket

--- Stocks.cpp
+++ Stocks.cpp
@@ -1,83 +1,248 @@
-/// ctrl+F for ////
-//read all comments
-//what if the constructor found an error how can it return that
+//you need that outside the main: string Stocks::comparing= "tickerSymbol";
 //Name: Abdulrahman Sahmoud
-
-#ifndef _STOCKS_H_
-#define _STOCKS_H_
-#include <string>
+///make sure i need to include which libraries
+#ifndef _STOCKS
+#define _STOCKS
+#include<iostream>
+#include<sstream>
 using namespace std;
-const int MAX_DAYS=30;//where to put that .cpp or .h? in private or outside
-class Stocks{
-	
-private:	
-	static string comparing;
-	int Ndays;//number of days for that company(will calculate that from the line im going to get)
-	string name;
-	string tickerSymbol;
-	unsigned long long marketCap[MAX_DAYS],volume[MAX_DAYS];
-	double high[MAX_DAYS],low[MAX_DAYS],open[MAX_DAYS],close[MAX_DAYS];
-
-    ///private member functions declarations
-	string getNthField(string str,int n,string seperator);
-	/*
-	   takes a generic array and the field with its colons
-	   loop through array and set it
-	*/
+#include "stocks.h"
+//string,string,double ,long long,long long,double,double,double
+//Name  ,Symbol,close  ,MarketCap,Volume   ,High   ,Low  ,Open
+//1     , 2    , 3     , 4       , 5       , 6     , 7   , 8  
+bool Stocks::add(double newclose,unsigned long long newmarketCap,unsigned long long newvolume,double newhigh,double newlow,double newopen){
+	if(Ndays==MAX_DAYS)
+		return false;
+	Ndays++;
+	close[Ndays]=newclose;
+	marketCap[Ndays]=newmarketCap;
+	volume[Ndays]=newvolume;
+	high[Ndays]=newhigh;
+	low[Ndays]=newlow;
+	open[Ndays]=newopen;
+	return true;
+}
+	string Stocks::comparableSTR(){
+		if(comparing=="name")
+			return name;
+		if(comparing=="tickerSymbol")
+			return tickerSymbol;
+		return "";
+	}
+	unsigned long long Stocks::comparableLL(){
+		if(comparing=="volume")
+			return volume[Ndays-1];
+		if(comparing=="marketCapital")
+			return marketCap[Ndays-1];
+		return -1;
+	}
+	double Stocks::comparableD(){
+		if(comparing=="close")
+			return close[Ndays-1];
+		if(comparing=="open")
+			return open[Ndays-1];
+		if(comparing=="high")
+			return high[Ndays-1];
+		if(comparing=="low")
+			return low[Ndays-1];
+		return -1;
+	}
+//----------------------------------------------------------------------//
+	string Stocks::getNthField(string line,int n,string seperator){
+		for(int i=0;i<n-1;i++)
+			line=line.substr(line.find(seperator)+1);
+		
+		return line.substr(0,line.find(seperator));
+	}
+//----------------------------------------------------------------------//
 	template<typename T>
-	string arrToString(T data[]);
-	
-	Stocks& copy(const Stocks&);//should it be constant?
-
-	string comparableSTR();
-	unsigned long long comparableLL();
-	double comparableD();
-
+	void Stocks::setArrayField(T data[],string str){
+		for(int i=1;i<=Ndays;i++){
+			stringstream ss( getNthField(str,i,":") );
+			ss>>data[i-1];
+		}
+	}
+//----------------------------------------------------------------------//
+	Stocks& Stocks::copy(const Stocks & rhs){
+		name=rhs.name;
+		tickerSymbol=rhs.tickerSymbol;
+		setArrayField(close,arrToString(rhs.close));
+		setArrayField(marketCap,arrToString(rhs.marketCap));
+		setArrayField(volume,arrToString(rhs.volume));
+		setArrayField(high,arrToString(rhs.high));
+		setArrayField(low,arrToString(rhs.low));
+		setArrayField(open,arrToString(rhs.open));
+		return *this;
+	}
+//----------------------------------------------------------------------//
+	Stocks::Stocks(const Stocks& rhs){
+		copy(rhs);
+	}
+	Stocks& Stocks::operator = (const Stocks& rhs){
+		return copy(rhs);
+	}
+//----------------------------------------------------------------------//
+	Stocks::Stocks(string line){
+		//set Ndays first
+		Ndays=1;
+		string tmp=getNthField(line,3,",");
+		for(int i=0;i<tmp.size();i++)
+			if(tmp[i]==':')
+				Ndays++;
+		if(Ndays>MAX_DAYS)Ndays=MAX_DAYS;
+		//setting the rest of the fields
+		name=getNthField(line,1,",");
+		tickerSymbol=getNthField(line,2,",");
+		setArrayField( close, getNthField(line,3,",") );///remember this returns false
+		setArrayField( marketCap, getNthField(line,4,",") );///remember this returns false
+		setArrayField( volume, getNthField(line,5,",") );///remember this returns false
+		setArrayField( high, getNthField(line,6,",") );///remember this returns false
+		setArrayField( low, getNthField(line,7,",") );///remember this returns false
+		setArrayField( open, getNthField(line,8,",") );///remember this returns false
+	}//end of Stocks(line)
 	template<typename T>
-	void setArrayField(T data[],string str);
-//--------------------public functions declarations--------------------//
-public:
-	Stocks(){}//do I really need this? its never going to get used
-	Stocks(const Stocks&);///should it be constant?
-	Stocks& operator = (const Stocks& rhs);///should it return const?
-	Stocks(string line);//line read from file passed to this constructor to construct an object
-	static void compareBy(string str){comparing=str;}
+	string Stocks::arrToString(T data[]){
+		string str;
+		for(int i=0;i<Ndays;i++){
+			stringstream ss;
+			ss<<data[i];
+			str+=ss.str()+":";
+		}
+		return str.substr(0,str.size()-1);
+	}
+	string Stocks::toString(){
+		string str;
+		str+=name+",";
+		str+=tickerSymbol+",";
+		str+=arrToString(close)+",";
+		str+=arrToString(marketCap)+",";
+		str+=arrToString(volume)+",";
+		str+=arrToString(high)+",";
+		str+=arrToString(low)+",";
+		str+=arrToString(open);
+		return str;
+	}//convert it back to string to the Breadth First Search can use it easily to output to the file
 
-	string toString();//convert it back to string to the Breadth First Search can use it easily to output to the file
-	bool add(double setclose,unsigned long long setmarketCap,unsigned long long setvolume,double high,double low,double open);
-	bool add(string line);
 // Comparison Operators
-	bool operator ==( Stocks& rhs);
-	bool operator > ( Stocks& rhs);
-	bool operator < (Stocks& rhs);
-	bool operator <= (Stocks& rhs);
-	bool operator != (Stocks& rhs);
+	bool Stocks::operator ==( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()==rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()==rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()==rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator > ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()>rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()>rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()>rhs.comparableLL();
+		return false;	
+	}
+	bool Stocks::operator <= ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()<=rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()<=rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()<=rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator < ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()<rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()<rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()<rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator != ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()!=rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()!=rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()!=rhs.comparableLL();
+		return false;
+	}
+	
 
-
-//--------------GETTERS----------------------//
-	///do i need to put const here?
-	string gettickerSymbol()const {return tickerSymbol;}
-	string getName()const {return name;}
-	int getNdays() const {return Ndays;}
-	//make sure in the function that the index is in bounds//
-	bool getClose(double & ret,int ind);
-	bool getHigh(double & ret,int ind);
-	bool getLow(double & ret,int ind);
-	bool getOpen(double & ret,int ind);
-	bool getmarketCap(unsigned long long & ret,int ind);
-	bool getVolume(unsigned long long & ret,int ind);
-
-
-////////////////////SETTERS////////////////////////////
-	void settickerSymbol(string set){tickerSymbol=set;}
-	void setName(string set){name=set;}
-	//make sure in the function that the index is in bounds//
-	bool setClose(double set,int ind);
-	bool setHigh(double set,int ind);
-	bool setLow(double set,int ind);
-	bool setOpen(double set,int ind);
-	bool setmarketCap(unsigned long long  set,int ind);
-	bool setVolume(unsigned long long  set,int ind);
-
-};
+//---------------------GETTERS-----------------------------//
+	bool Stocks::getClose(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=close[ind];
+		return true;
+	}
+	bool Stocks::getHigh(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=high[ind];
+		return true;
+	}
+	bool Stocks::getLow(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=low[ind];
+		return true;
+	}
+	bool Stocks::getOpen(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=open[ind];
+		return true;
+	}
+	bool Stocks::getmarketCap(unsigned long long & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=marketCap[ind];
+		return true;
+	}
+	bool Stocks::getVolume(unsigned long long & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=volume[ind];
+		return true;
+	}
+//-----------------SETTERS---------------------------------//
+	bool Stocks::setClose(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		close[ind]=set;
+		return true;
+	}
+	bool Stocks::setHigh(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		high[ind]=set;
+		return true;
+	}
+	bool Stocks::setLow(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		low[ind]=set;
+		return true;
+	}
+	bool Stocks::setOpen(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		open[ind]=set;
+		return true;
+	}
+	bool Stocks::setmarketCap(unsigned long long  set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		marketCap[ind]=set;
+		return true;
+	}
+	bool Stocks::setVolume(unsigned long long  set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		volume[ind]=set;
+		return true;
+	}
 #endif
