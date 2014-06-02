d59bf7f18b3e2b585b04d68d70b302319c11cbd8
Abdulrahman Sahmoud <slashhashdash@gmail.com>
2014-06-02 02:44:59 +0000

    you need that line outside the main string Stocks::comparing= "tickerSymbol"; then whenever you want to change it Stocks::comparing= "open"; field names: name,tickerSymbol,volume,marketCapital,close,open,high,low

---
+++ Stocks.cpp
@@ -0,0 +1,234 @@
+//Name: Abdulrahman Sahmoud
+//make sure i need to include which libraries
+#ifndef _STOCKS
+#define _STOCKS
+#include<iostream>
+#include<sstream>
+using namespace std;
+#include "stocks.h"
+//string,string,double ,long long,long long,double,double,double
+//Name  ,Symbol,close  ,MarketCap,Volume   ,High   ,Low  ,Open
+//1     , 2    , 3     , 4       , 5       , 6     , 7   , 8  
+	string Stocks::comparableSTR(){
+		if(comparing=="name")
+			return name;
+		if(comparing=="tickerSymbol")
+			return tickerSymbol;
+		return "";
+	}
+	long long Stocks::comparableLL(){
+		if(comparing=="volume")
+			return volume[Ndays-1];
+		if(comparing=="marketCapital")
+			return marketCap[Ndays-1];
+		return -1;
+	}
+	double Stocks::comparableD(){
+		if(comparing=="close")
+			return close[Ndays-1];
+		if(comparing=="open")
+			return open[Ndays-1];
+		if(comparing=="high")
+			return high[Ndays-1];
+		if(comparing=="low")
+			return low[Ndays-1];
+		return -1;
+	}
+//----------------------------------------------------------------------//
+	string Stocks::getNthField(string line,int n,string seperator){
+		for(int i=0;i<n-1;i++)
+			line=line.substr(line.find(seperator)+1);
+		
+		return line.substr(0,line.find(seperator));
+	}
+//----------------------------------------------------------------------//
+	template<typename T>
+	void Stocks::setArrayField(T data[],string str){
+		for(int i=1;i<=Ndays;i++){
+			stringstream ss( getNthField(str,i,":") );
+			ss>>data[i-1];
+		}
+	}
+//----------------------------------------------------------------------//
+	Stocks& Stocks::copy(const Stocks & rhs){
+		name=rhs.name;
+		tickerSymbol=rhs.tickerSymbol;
+		setArrayField(close,arrToString(rhs.close));
+		setArrayField(marketCap,arrToString(rhs.marketCap));
+		setArrayField(volume,arrToString(rhs.volume));
+		setArrayField(high,arrToString(rhs.high));
+		setArrayField(low,arrToString(rhs.low));
+		setArrayField(open,arrToString(rhs.open));
+		return *this;
+	}
+//----------------------------------------------------------------------//
+	Stocks::Stocks(const Stocks& rhs){
+		copy(rhs);
+	}
+	Stocks& Stocks::operator = (const Stocks& rhs){
+		return copy(rhs);
+	}
+//----------------------------------------------------------------------//
+	Stocks::Stocks(string line){
+		//set Ndays first
+		Ndays=1;
+		string tmp=getNthField(line,3,",");
+		for(int i=0;i<tmp.size();i++)
+			if(tmp[i]==':')
+				Ndays++;
+		//setting the rest of the fields
+		name=getNthField(line,1,",");
+		tickerSymbol=getNthField(line,2,",");
+		setArrayField( close, getNthField(line,3,",") );///remember this returns false
+		setArrayField( marketCap, getNthField(line,4,",") );///remember this returns false
+		setArrayField( volume, getNthField(line,5,",") );///remember this returns false
+		setArrayField( high, getNthField(line,6,",") );///remember this returns false
+		setArrayField( low, getNthField(line,7,",") );///remember this returns false
+		setArrayField( open, getNthField(line,8,",") );///remember this returns false
+	}//end of Stocks(line)
+	template<typename T>
+	string Stocks::arrToString(T data[]){
+		string str;
+		for(int i=0;i<Ndays;i++){
+			stringstream ss;
+			ss<<data[i];
+			str+=ss.str()+":";
+		}
+		return str.substr(0,str.size()-1);
+	}
+	string Stocks::toString(){
+		string str;
+		str+=name+",";
+		str+=tickerSymbol+",";
+		str+=arrToString(close)+",";
+		str+=arrToString(marketCap)+",";
+		str+=arrToString(volume)+",";
+		str+=arrToString(high)+",";
+		str+=arrToString(low)+",";
+		str+=arrToString(open);
+		return str;
+	}//convert it back to string to the Breadth First Search can use it easily to output to the file
+
+// Comparison Operators
+	bool Stocks::operator ==( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()==rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()==rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()==rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator > ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()>rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()>rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()>rhs.comparableLL();
+		return false;	
+	}
+	bool Stocks::operator <= ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()<=rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()<=rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()<=rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator < ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()<rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()<rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()<rhs.comparableLL();
+		return false;
+	}
+	bool Stocks::operator != ( Stocks& rhs){
+		if(comparing=="name" || comparing=="tickerSymbol")
+			return comparableSTR()!=rhs.comparableSTR();
+		if(comparing=="open"||comparing=="close"||comparing=="high"||comparing=="low")
+			return comparableD()!=rhs.comparableD();
+		if(comparing=="volume"||comparing=="marketCapital")
+			return comparableLL()!=rhs.comparableLL();
+		return false;
+	}
+	
+
+//---------------------GETTERS-----------------------------//
+	bool Stocks::getClose(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=close[ind];
+		return true;
+	}
+	bool Stocks::getHigh(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=high[ind];
+		return true;
+	}
+	bool Stocks::getLow(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=low[ind];
+		return true;
+	}
+	bool Stocks::getOpen(double & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=open[ind];
+		return true;
+	}
+	bool Stocks::getmarketCap(long long & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=marketCap[ind];
+		return true;
+	}
+	bool Stocks::getVolume(long long & ret,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		ret=volume[ind];
+		return true;
+	}
+//-----------------SETTERS---------------------------------//
+	bool Stocks::setClose(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		close[ind]=set;
+		return true;
+	}
+	bool Stocks::setHigh(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		high[ind]=set;
+		return true;
+	}
+	bool Stocks::setLow(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		low[ind]=set;
+		return true;
+	}
+	bool Stocks::setOpen(double set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		open[ind]=set;
+		return true;
+	}
+	bool Stocks::setmarketCap(long long  set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		marketCap[ind]=set;
+		return true;
+	}
+	bool Stocks::setVolume(long long  set,int ind){
+		if(ind<0||ind>Ndays)
+			return false;
+		volume[ind]=set;
+		return true;
+	}
+#endif
