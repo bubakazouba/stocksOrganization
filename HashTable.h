ac81857449ce7c50307a737b34faa6e431e56513
Tariq Anees <trqanees94@gmail.com>
2014-06-16 22:36:23 +0000

    HashTable.h edited online with Bitbucket

--- HashTable.h
+++ HashTable.h
@@ -11,23 +11,22 @@ class HashTable{
 
 public:
 
-   bool find(Stocks* temp, Stocks*& returnTemp); //return refernce to object
-   void sortedList();//pointer to a queue
+   bool find(Stocks* temp, Stocks*& returnTemp); //return refernce to object so that the Controller can delete the object inside array
 
-   void hashStats();
-   void List(Queue<Stocks*>& StockQueue);
+   void hashStats(); // outputs the stats
+   void List(Queue<Stocks*>& StockQueue);// a queue is implemented 
 
-   bool add(Stocks* temp);
-   bool remove(Stocks*& returnTemp);
+   bool add(Stocks* temp); // temp is an object with one key field
+   bool remove(Stocks*& returnTemp); //return gets modified with the actual data of the object
 
 
-HashTable(int size);
+HashTable(int size); //size is the # data records
 ~HashTable();
 
 
 private:
 
-int arrSize;
+int arrSize;//these 5 variables are updatted as remove and add are called
 int maxProbe;
 int numStocks;
 int numZeroProbes;
@@ -35,7 +34,7 @@ int numCollisions;
 
   Stocks** hashArray;
 
-  bool myFind(string key );
+  bool myFind(string key ); //private find function used only by the remove, it has less error checks
   int collRes(int indexRes, int numProbes);
   int indexGenerator( string ticker);
 
